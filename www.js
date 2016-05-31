b3e65c980bab1d7fbe30954817a539cf9a8721e5
vingo <maoobo@163.com>
2016-05-31 11:04:57 +0800

    clear empty line

--- www.js
+++ www.js
@@ -41,8 +41,6 @@ app.get('/data', function(req, res) {
     // });
     //res.set('Content-Type', 'text/plain');
     res.set('Content-Type', 'application/json');
-
-
     res.send(data);
     //res.jsonp(data);
 });
